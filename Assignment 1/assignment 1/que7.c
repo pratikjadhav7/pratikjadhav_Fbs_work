bebc738c57b8ef3770fe6ace509fc35c2b3720e2
pratikjadhav7 <pratikjadhav9302@gmail.com>
2026-01-16 19:42:55 +0530

    Add files via upload

---
+++ Assignment 1/assignment 1/que7.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int salary=5000;
+    float da,ta,rha,total;
+    if(salary<=5000){
+        da=(10.0/100)*salary;
+        ta=(20.0/100)*salary;
+        rha=(25.0/100)*salary;
+}else{
+        da=(15.0/100)*salary;
+        ta=(25.0/100)*salary;
+        rha=(30.0/100)*salary;
+
+}
+total=da+ta+rha;
+printf("da is %f\n",da);
+printf("ta is %f\n",ta);
+printf("rha is %f\n",rha);
+printf("Total will be: %f",total);
+    
+}
