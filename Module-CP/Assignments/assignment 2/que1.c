dbcdd12ac37f177634fae46ae634c58c8eeed8e9
pratikjadhav7 <pratikjadhav9302@gmail.com>
2026-01-16 23:57:02 +0530

    Add files via upload

---
+++ Module-CP/Assignments/assignment 2/que1.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+    int num1,num2;
+    char op;
+    float res;
+    printf("Enter 1st Num:");
+    scanf("%d",&num1);
+    printf("Enter 2nd Num:");
+    scanf("%d",&num2);
+    printf("Enter op to perform operation:");
+    scanf(" %c",&op);
+    if(op=='+'){
+        res = num1+num2;
+        printf("%d + %d = %f",num1,num2,res);
+
+    }else{
+        if(op=='-'){
+            res = num1-num2;
+                printf("%d - %d = %f",num1,num2,res);
+        }else{
+            if(op=='%'){
+                res = num1%num2;
+                    printf("%d / %d = %f",num1,num2,res);
+            }else{
+               if(num1*num2){
+                res = num1*num2;
+                    printf("%d * %d = %f",num1,num2,res);
+               } 
+            }
+        }
+    }
+}
---
+++ Module-CP/Assignments/assignment 2/que3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+    int main(){
+        int num1,num2,num3;
+        printf("Enter the numbers:\n");
+        printf("Num1:");
+        scanf("%d",&num1);
+        printf("Num2:");
+        scanf("%d",&num2);
+        printf("Num3:");
+        scanf("%d",&num3);
+        if(num1>num2){
+            if(num1>num3){
+                printf("%d is greater than %d and %d",num1,num2,num3);
+            }
+        }
+        if(num2>num1){
+            if(num2>num3){
+                printf("%d is greater than %d and %d",num2,num1,num3);
+            }
+        }
+        if(num3>num1){
+            if(num3>num2){
+                printf("%d is greater than %d and %d",num3,num1,num2);
+            }
+        }
+
+    }
