4f274e54451e4171889010eb6253dd9f4ac5b996
pratikjadhav7 <pratikjadhav9302@gmail.com>
2026-01-18 14:42:25 +0530

    Update que3.c

--- Module-CP/Assignments/assignment 1/que3.c
+++ Module-CP/Assignments/assignment 1/que3.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main(){
     int year = 2004;
-    if(year%4==0){
+    if(year%4==0 && year%100!==0 || year%400==0){
         printf("%d is leap year",year);
     }else{
         printf("%d is not leap year",year);
     }
+
 }
