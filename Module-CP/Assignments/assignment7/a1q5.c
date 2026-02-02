5e873e20e6300a73a576613c5f109891ccf324c4
pratikjadhav7 <pratikjadhav9302@gmail.com>
2026-02-02 18:09:16 +0530

    Add files via upload

---
+++ Module-CP/Assignments/assignment7/a1q5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void isUpper(char*ch);
+int main(){
+     char ch = 'X';
+    isUpper(&ch);
+   
+}
+void isUpper(char *ch){
+    if(*ch>='A' && *ch<='Z'){
+        printf("%c is Uppercase!",*ch);
+    }else{
+        printf("%c is Lowercase!",*ch);
+    }
+}
