51f5689593fc4a634cc75e66e20c5d2566b2434b
pratikjadhav7 <pratikjadhav9302@gmail.com>
2026-02-09 08:20:54 +0530

    Add files via upload

---
+++ Module-CP/Assignments/assignment7/a2q6.c
@@ -0,0 +1,26 @@
+
+#include<stdio.h>
+void isDivisible(int*);
+int main(){
+    int num;
+    printf("Enter the Number:");
+    scanf("%d",&num);
+    isDivisible(&num);
+
+}
+void isDivisible(int *num){
+    if(*num%3==0 && *num%5==0){
+       printf("%d is divisible by both.",*num);
+    }
+    else{
+        if(*num%3==0 && *num%5!=0){
+            printf("%d is divisible by 3 not 5.",*num);
+        }else{
+            if(*num%5==0 && *num%3!=0){
+                printf("%d is divisible by 5 not 3.",*num);
+            }else{
+                printf("%d Divisible by none.",*num);
+            }
+        }
+    }
+}
