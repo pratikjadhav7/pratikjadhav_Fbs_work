d22c29752e89eaf9cf32f25f0699188931463329
pratikjadhav7 <pratikjadhav9302@gmail.com>
2026-03-23 22:20:34 +0530

    Delete Module-Core Java directory

--- Module-Core Java/Assigmmemts/Assignment 0/q7.java
+++
@@ -1,9 +0,0 @@
-public class q7{
-    public static void main(String[] args) {
-        int mins = 3004;
-    int hrs = mins/10;
-    int hr = hrs/60;
-    int min = mins%10;
-    System.out.printf("%d hours and %d mins",hr,min);
-    }
-}
